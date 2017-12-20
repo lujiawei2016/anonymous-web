0768ec8867fd4e6422d268c7c39341b6e69efb18
lujiawei <lujiawei2015@sina.com>
2017-12-20 23:12:48 +0800

    与fastdfs整合文件上传功能

---
+++ src/main/java/com/anonymous/fastdfs/FileManagerConfig.java
@@ -0,0 +1,18 @@
+package com.anonymous.fastdfs;
+
+import java.io.Serializable;
+
+public interface FileManagerConfig extends Serializable {
+
+	public static final String FILE_DEFAULT_AUTHOR = "lujiawei";
+
+    public static final String PROTOCOL = "http://";
+
+    public static final String SEPARATOR = "/";
+
+    public static final String TRACKER_NGNIX_ADDR = "192.168.11.129";
+
+    public static final String TRACKER_NGNIX_PORT = "";
+
+    public static final String CLIENT_CONFIG_FILE = "fdfs_client.conf";
+}
--- src/main/java/com/anonymous/test/controller/USController.java
+++ src/main/java/com/anonymous/test/controller/USController.java
@@ -1,12 +1,22 @@
 package com.anonymous.test.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.csource.common.MyException;
+import org.csource.common.NameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.anonymous.fastdfs.FastDFSFile;
+import com.anonymous.fastdfs.FileManager;
 import com.anonymous.test.pojo.Article;
 import com.anonymous.test.pojo.User;
 import com.anonymous.test.service.IUserService;
@@ -42,5 +52,22 @@ public class USController {
 		Article article = userService.getArticleById(1);
 		System.out.println(article);
 		return article;
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public String add(@RequestParam MultipartFile attach, HttpServletRequest request)
+	        throws IOException, MyException {
+	    // 获取文件后缀名 
+	    String ext = attach.getOriginalFilename().substring(attach.getOriginalFilename().lastIndexOf(".")+1);
+	    FastDFSFile file = new FastDFSFile(attach.getBytes(),ext);
+	    NameValuePair[] meta_list = new NameValuePair[4];
+	    meta_list[0] = new NameValuePair("fileName", attach.getOriginalFilename());
+	    meta_list[1] = new NameValuePair("fileLength", String.valueOf(attach.getSize()));
+	    meta_list[2] = new NameValuePair("fileExt", ext);
+	    meta_list[3] = new NameValuePair("fileAuthor", "lujiawei");
+	    String filePath = FileManager.upload(file,meta_list);
+	    System.out.println(filePath);
+	    return filePath;
 	}
 }
