afc992a0aae8a898cfe58cd758cdae2b1a78ce2a
lujiawei <lujiawei2015@sina.com>
2018-01-08 17:42:45 +0800

    增加注册功能

--- src/main/java/com/anonymous/message/service/MessageService.java
+++ src/main/java/com/anonymous/message/service/MessageService.java
@@ -15,4 +15,14 @@ public interface MessageService {
 	 * @throws Exception
 	 */
 	public Object sendLoginMsg(String phone) throws Exception;
+	
+	/**
+	 * 快捷登陆
+	 * @param phone
+	 * @param code
+	 * @param deviceId
+	 * @return
+	 * @throws Exception
+	 */
+	public Object quickLogin(String phone,String code,String deviceId) throws Exception;
 }
