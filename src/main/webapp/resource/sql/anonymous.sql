0296d21d1007504b499b8b90ebbe5f937e1ba668
Administrator <Administrator@SC-201707281455>
2018-01-20 12:58:52 +0800

    更新sql数据库，支持emoji表情存储

--- src/main/webapp/resource/sql/anonymous.sql
+++ src/main/webapp/resource/sql/anonymous.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.24 (32 bit)
-MySQL - 5.5.20 : Database - anonymous
+MySQL - 5.7.21 : Database - anonymous
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 5.5.20 : Database - anonymous
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`anonymous` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`anonymous` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `anonymous`;
 
@@ -21,42 +21,42 @@ USE `anonymous`;
 DROP TABLE IF EXISTS `an_anonym`;
 
 CREATE TABLE `an_anonym` (
-  `anonymId` varchar(100) NOT NULL COMMENT '主键',
-  `nickName` varchar(100) DEFAULT NULL COMMENT '昵称',
-  `userName` varchar(100) DEFAULT NULL COMMENT '账号',
-  `password` varchar(100) DEFAULT NULL COMMENT '密码',
-  `headerImg` varchar(255) DEFAULT NULL COMMENT '头像',
-  `phone` varchar(50) DEFAULT NULL COMMENT '手机号码',
+  `anonymId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `nickName` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '昵称',
+  `userName` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '账号',
+  `password` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '密码',
+  `headerImg` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '头像',
+  `phone` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '手机号码',
   `isLock` int(5) DEFAULT '1' COMMENT '是否锁定，1-未锁定，0-已锁定',
-  `deviceId` varchar(255) DEFAULT NULL COMMENT '设备唯一标识',
-  `lockReason` varchar(255) DEFAULT NULL COMMENT '锁定理由',
+  `deviceId` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '设备唯一标识',
+  `lockReason` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '锁定理由',
   `delFlag` int(5) DEFAULT '1' COMMENT '删除标识，1-未删除，0-已删除',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`anonymId`),
   KEY `anonym_phone` (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_anonym` */
 
-insert  into `an_anonym`(`anonymId`,`nickName`,`userName`,`password`,`headerImg`,`phone`,`isLock`,`deviceId`,`lockReason`,`delFlag`,`createTime`,`updateTime`) values ('b5ae7b0d-a7df-4a9d-86c5-657b355d8f39','张三','18320172397',NULL,'','18320172397',1,'864664034884823',NULL,1,'2018-01-08 22:46:30','2018-01-08 22:46:30');
+insert  into `an_anonym`(`anonymId`,`nickName`,`userName`,`password`,`headerImg`,`phone`,`isLock`,`deviceId`,`lockReason`,`delFlag`,`createTime`,`updateTime`) values ('e4295540-c3e7-44f2-bbae-2ee6d30d5a13','张三','18320172397',NULL,'','18320172397',1,'864664034884823',NULL,1,'2018-01-20 10:34:19','2018-01-20 10:34:19');
 
 /*Table structure for table `an_anonymreport` */
 
 DROP TABLE IF EXISTS `an_anonymreport`;
 
 CREATE TABLE `an_anonymreport` (
-  `reportId` varchar(100) NOT NULL COMMENT '主键',
-  `passiveAnonymId` varchar(100) NOT NULL COMMENT '被举报用户id',
-  `activeAnonymId` varchar(100) NOT NULL COMMENT '举报用户id',
-  `reportReason` longtext COMMENT '举报理由',
+  `reportId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `passiveAnonymId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '被举报用户id',
+  `activeAnonymId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '举报用户id',
+  `reportReason` longtext CHARACTER SET utf8 COMMENT '举报理由',
   `isHandle` int(5) DEFAULT NULL COMMENT '是否处理，1-已处理，0-未处理',
-  `reportResult` longtext COMMENT '处理结果',
+  `reportResult` longtext CHARACTER SET utf8 COMMENT '处理结果',
   `repotHandleTime` datetime DEFAULT NULL COMMENT '处理时间',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`reportId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_anonymreport` */
 
@@ -65,38 +65,40 @@ CREATE TABLE `an_anonymreport` (
 DROP TABLE IF EXISTS `an_card`;
 
 CREATE TABLE `an_card` (
-  `cardId` varchar(100) NOT NULL COMMENT '主键',
-  `cardContent` longtext NOT NULL COMMENT '卡片内容',
-  `cardImg` varchar(255) DEFAULT NULL COMMENT '卡片图片',
-  `isLock` int(5) DEFAULT NULL COMMENT '是否锁定，1-未锁定，0-已锁定',
-  `lockReason` longtext COMMENT '锁定理由',
+  `cardId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `cardContent` varchar(255) NOT NULL,
+  `cardImg` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '卡片图片',
+  `isLock` int(5) DEFAULT '1' COMMENT '是否锁定，1-未锁定，0-已锁定',
+  `lockReason` longtext CHARACTER SET utf8 COMMENT '锁定理由',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `delFlag` int(5) DEFAULT NULL COMMENT '删除标识，1-未删除，0-已删除',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `delFlag` int(5) DEFAULT '1' COMMENT '删除标识，1-未删除，0-已删除',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`cardId`),
   KEY `card_anonymId` (`anonymId`),
   CONSTRAINT `card_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_card` */
+
+insert  into `an_card`(`cardId`,`cardContent`,`cardImg`,`isLock`,`lockReason`,`createTime`,`updateTime`,`delFlag`,`anonymId`) values ('2fe0a9c4-cde7-4084-aac1-22cdf6214a3c','哈哈哈哈哈哈哈?????你摸摸女号绿卡饿了紧张????','http://192.168.16.128:9999/group1/M00/00/00/wKgQgFpjL-mAXiIeAACOI1ye29A5910116',1,NULL,'2018-01-20 12:02:56','2018-01-20 12:02:56',1,'e4295540-c3e7-44f2-bbae-2ee6d30d5a13');
 
 /*Table structure for table `an_cardcollection` */
 
 DROP TABLE IF EXISTS `an_cardcollection`;
 
 CREATE TABLE `an_cardcollection` (
-  `cardCollectiionId` varchar(100) NOT NULL COMMENT '主键',
+  `cardCollectiionId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
-  `cardId` varchar(100) DEFAULT NULL COMMENT '卡片id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
+  `cardId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '卡片id',
   PRIMARY KEY (`cardCollectiionId`),
   KEY `collection_anonymId` (`anonymId`),
   KEY `collection_cardId` (`cardId`),
   CONSTRAINT `collection_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `collection_cardId` FOREIGN KEY (`cardId`) REFERENCES `an_card` (`cardId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_cardcollection` */
 
@@ -105,20 +107,20 @@ CREATE TABLE `an_cardcollection` (
 DROP TABLE IF EXISTS `an_cardcomment`;
 
 CREATE TABLE `an_cardcomment` (
-  `cardCommentId` varchar(100) NOT NULL COMMENT '主键',
-  `cardCommentContent` varchar(255) NOT NULL COMMENT '评论内容',
-  `carCommentReplyId` varchar(100) DEFAULT NULL COMMENT '回复评论id',
+  `cardCommentId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `cardCommentContent` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '评论内容',
+  `carCommentReplyId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '回复评论id',
   `delFlag` int(5) DEFAULT NULL COMMENT '删除标识，1-未删除，0-已删除',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
-  `cardId` varchar(100) DEFAULT NULL COMMENT '卡片id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
+  `cardId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '卡片id',
   PRIMARY KEY (`cardCommentId`),
   KEY `comment_cardId` (`cardId`),
   KEY `comment_anonymId` (`anonymId`),
   CONSTRAINT `comment_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `comment_cardId` FOREIGN KEY (`cardId`) REFERENCES `an_card` (`cardId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_cardcomment` */
 
@@ -127,17 +129,17 @@ CREATE TABLE `an_cardcomment` (
 DROP TABLE IF EXISTS `an_cardcommentfabulous`;
 
 CREATE TABLE `an_cardcommentfabulous` (
-  `cardCommentFabulousId` varchar(100) NOT NULL COMMENT '主键',
+  `cardCommentFabulousId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `cardCommentId` varchar(100) DEFAULT NULL COMMENT '评论id',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `cardCommentId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '评论id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`cardCommentFabulousId`),
   KEY `cardcommentfabulous_anonymId` (`anonymId`),
   KEY `cardcommentfabulous_cardCommentId` (`cardCommentId`),
   CONSTRAINT `cardcommentfabulous_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `cardcommentfabulous_cardCommentId` FOREIGN KEY (`cardCommentId`) REFERENCES `an_cardcomment` (`cardCommentId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_cardcommentfabulous` */
 
@@ -146,17 +148,17 @@ CREATE TABLE `an_cardcommentfabulous` (
 DROP TABLE IF EXISTS `an_cardfabulous`;
 
 CREATE TABLE `an_cardfabulous` (
-  `cardFabulousId` varchar(100) NOT NULL COMMENT '主键',
+  `cardFabulousId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `cardId` varchar(100) DEFAULT NULL COMMENT '卡片id',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `cardId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '卡片id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`cardFabulousId`),
   KEY `cardfabulous_anonymId` (`anonymId`),
   KEY `cardfabulous_cardId` (`cardId`),
   CONSTRAINT `cardfabulous_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `cardfabulous_cardId` FOREIGN KEY (`cardId`) REFERENCES `an_card` (`cardId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_cardfabulous` */
 
@@ -165,18 +167,18 @@ CREATE TABLE `an_cardfabulous` (
 DROP TABLE IF EXISTS `an_story`;
 
 CREATE TABLE `an_story` (
-  `storyId` varchar(100) NOT NULL COMMENT '主键',
-  `storyTitle` varchar(200) DEFAULT NULL COMMENT '标题',
-  `storyContent` longtext COMMENT '内容',
+  `storyId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `storyTitle` varchar(200) CHARACTER SET utf8 DEFAULT NULL COMMENT '标题',
+  `storyContent` longtext CHARACTER SET utf8 COMMENT '内容',
   `isLock` int(5) DEFAULT NULL COMMENT '是否锁定，1-未锁定，0-已锁定',
-  `lockReason` longtext COMMENT '锁定理由',
+  `lockReason` longtext CHARACTER SET utf8 COMMENT '锁定理由',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '作者（即用户）',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '作者（即用户）',
   PRIMARY KEY (`storyId`),
   KEY `story_anonymId` (`anonymId`),
   CONSTRAINT `story_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_story` */
 
@@ -185,17 +187,17 @@ CREATE TABLE `an_story` (
 DROP TABLE IF EXISTS `an_storycollection`;
 
 CREATE TABLE `an_storycollection` (
-  `storyCollectiionId` varchar(100) NOT NULL COMMENT '主键',
+  `storyCollectiionId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
-  `storyId` varchar(100) DEFAULT NULL COMMENT '故事id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
+  `storyId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '故事id',
   PRIMARY KEY (`storyCollectiionId`),
   KEY `storycollection_storyId` (`storyId`),
   KEY `storycollection_anonymId` (`anonymId`),
   CONSTRAINT `storycollection_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `storycollection_storyId` FOREIGN KEY (`storyId`) REFERENCES `an_story` (`storyId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_storycollection` */
 
@@ -204,20 +206,20 @@ CREATE TABLE `an_storycollection` (
 DROP TABLE IF EXISTS `an_storycomment`;
 
 CREATE TABLE `an_storycomment` (
-  `storyCommentId` varchar(100) NOT NULL COMMENT '主键',
-  `storyCommentContent` varchar(255) DEFAULT NULL COMMENT '评论内容',
-  `styroReplyCommentId` varchar(100) DEFAULT NULL COMMENT '回复故事的id，不是回复的评论为null',
+  `storyCommentId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `storyCommentContent` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '评论内容',
+  `styroReplyCommentId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '回复故事的id，不是回复的评论为null',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
   `delFlag` int(5) DEFAULT NULL COMMENT '删除标识，1-未删除，0-未删除',
-  `storyId` varchar(100) DEFAULT NULL COMMENT '故事id',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `storyId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '故事id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`storyCommentId`),
   KEY `storycomment_anonymId` (`anonymId`),
   KEY `storycomment_storyId` (`storyId`),
   CONSTRAINT `storycomment_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `storycomment_storyId` FOREIGN KEY (`storyId`) REFERENCES `an_story` (`storyId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_storycomment` */
 
@@ -226,17 +228,17 @@ CREATE TABLE `an_storycomment` (
 DROP TABLE IF EXISTS `an_storycommentfabulous`;
 
 CREATE TABLE `an_storycommentfabulous` (
-  `StoryCommentFabulousId` varchar(100) NOT NULL COMMENT '主键',
+  `StoryCommentFabulousId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `storyCommentId` varchar(100) DEFAULT NULL COMMENT '评论id',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `storyCommentId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '评论id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`StoryCommentFabulousId`),
   KEY `storycommentfabulous_anonymId` (`anonymId`),
   KEY `storycommentfabulous_storyCommentId` (`storyCommentId`),
   CONSTRAINT `storycommentfabulous_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `storycommentfabulous_storyCommentId` FOREIGN KEY (`storyCommentId`) REFERENCES `an_storycomment` (`storyCommentId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_storycommentfabulous` */
 
@@ -245,17 +247,17 @@ CREATE TABLE `an_storycommentfabulous` (
 DROP TABLE IF EXISTS `an_storyfabulous`;
 
 CREATE TABLE `an_storyfabulous` (
-  `storyFabulousId` varchar(100) NOT NULL COMMENT '主键',
+  `storyFabulousId` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT '主键',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `storyId` varchar(100) DEFAULT NULL COMMENT '故事id',
-  `anonymId` varchar(100) DEFAULT NULL COMMENT '用户id',
+  `storyId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '故事id',
+  `anonymId` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT '用户id',
   PRIMARY KEY (`storyFabulousId`),
   KEY `storyfabulous_anonymId` (`anonymId`),
   KEY `storyfabulous_storyId` (`storyId`),
   CONSTRAINT `storyfabulous_anonymId` FOREIGN KEY (`anonymId`) REFERENCES `an_anonym` (`anonymId`),
   CONSTRAINT `storyfabulous_storyId` FOREIGN KEY (`storyId`) REFERENCES `an_story` (`storyId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `an_storyfabulous` */
 
