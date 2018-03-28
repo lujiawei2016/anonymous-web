d3c54f36c3a2e7feaa61dc23ed4191dbbbfaba7c
Administrator <Administrator@SC-201707281455>
2018-03-28 23:24:08 +0800

    用户头像上传功能

--- src/main/java/com/anonymous/anonym/controller/AnonymController.java
+++ src/main/java/com/anonymous/anonym/controller/AnonymController.java
@@ -1,14 +1,31 @@
 package com.anonymous.anonym.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.anonymous.anonym.service.AnonymService;
 import com.anonymous.custom.annotation.IdentityCheck;
+import com.anonymous.utils.FileUtils;
 
+import net.coobird.thumbnailator.Thumbnails;
 import net.sf.json.JSONObject;
 
 /**
@@ -21,6 +38,8 @@ import net.sf.json.JSONObject;
 @RequestMapping(value="/anonym")
 public class AnonymController {
 	
+	private static final Logger logger = Logger.getLogger(AnonymController.class);
+	
 	@Autowired
 	private AnonymService anonymService;
 
@@ -29,7 +48,7 @@ public class AnonymController {
 	 * @param anonymId
 	 * @return
 	 */
-	@RequestMapping(value="/getAnonymInfoById/{anonymId}")
+	@RequestMapping(value="/getAnonymInfoById/{anonymId}",method=RequestMethod.POST)
 	@ResponseBody
 	@IdentityCheck(check=false)
 	public Object getAnonymInfoById(@PathVariable String anonymId){
@@ -42,4 +61,69 @@ public class AnonymController {
 		return null;
 	}
 	
+	/**
+	 * 上传头像并保存
+	 * @param file
+	 * @param anonymId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value="/uploadHeadImg",method=RequestMethod.POST)
+	@ResponseBody
+	@IdentityCheck
+	public Object uploadHeadImg(@RequestParam MultipartFile file,String anonymId,
+			HttpServletRequest request,HttpServletResponse response){
+		try {
+			
+			response.setCharacterEncoding("utf-8");
+	        request.setCharacterEncoding("utf-8");
+	        
+	        String picName = null;//最终名字
+	        
+	        String realPath = request.getSession().getServletContext().getRealPath(""+File.separator+"uploadImage");
+	        if(!(file.isEmpty())){
+	        	
+	        	String ext = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+	        	if(!StringUtils.isBlank(ext) && ("jpg".equals(ext)) || "jpeg".equals(ext) || "png".equals(ext) || "bmp".equals(ext)){
+	        		//获取图片属性并保存
+	                picName = UUID.randomUUID().toString();
+	                
+	                File targetFile = new File(realPath,picName+"."+ext);
+	                file.transferTo(targetFile);
+	                
+	                if("png".equals(ext)){
+	                	Thumbnails.of(realPath+File.separator+picName+"."+ext).outputFormat("jpg").scale(1f).outputQuality(0.25f).toFile(realPath+File.separator+picName+"_fuben.jpg");
+	                }else{
+	                	Thumbnails.of(realPath+File.separator+picName+"."+ext).scale(1f).outputQuality(0.25f).toFile(realPath+File.separator+picName+"_fuben.jpg");
+	                }
+	                
+	                File fastdfsFile = new File(realPath,picName+"_fuben.jpg");
+	                
+	                FileInputStream inputStream = new FileInputStream(fastdfsFile);
+	                MultipartFile multipartFile = new MockMultipartFile(picName+"_fuben.jpg", inputStream);
+	                
+	                String imgPath = FileUtils.upload(multipartFile);
+	                
+	                System.out.println(imgPath);
+	    			
+	    			logger.info("文件上传成功");
+	                
+	    			return "{\"name\":\"abc\"}";
+	        	}else{
+	        		Map<String, Object> resultMap = new HashMap<>();
+	        		resultMap.put("result", "00");
+	        		resultMap.put("msg", "图片格式错误");
+	        		Object result = JSONObject.fromObject(resultMap);
+	        		return result;
+	        	}
+                
+            }
+			
+		} catch (Exception e) {
+			logger.error("发布卡片异常，异常信息为："+e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
